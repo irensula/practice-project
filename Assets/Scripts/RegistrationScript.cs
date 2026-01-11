86b8b4ad25d42883ed992d5895cbb3707b48adc1
irensula <irinawebdev@gmail.com>
2026-01-11 15:00:50 +0200

    add db.json

--- Assets/Scripts/RegistrationScript.cs
+++ Assets/Scripts/RegistrationScript.cs
@@ -1,15 +1,50 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using System.IO;
 using TMPro;
+
+[System.Serializable]
+public class UserData
+{
+    public string email;
+    public string password;
+}
+
+[System.Serializable]
+public class Database
+{
+    public UserData user;
+}
 public class RegistrationScript : MonoBehaviour
 {
     public TMP_InputField inputEmail;
     public TMP_InputField inputPassword;
 
-    string defaultEmail = "user";
-    string defaultPassword = "password";
+    private string defaultEmail;
+    private string defaultPassword;
+
+    void Start()
+    {
+        LoadUserData();
+    }
+
+    void LoadUserData()
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, "db.json");
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            Database db = JsonUtility.FromJson<Database>(json);
+
+            defaultEmail = db.user.email;
+            defaultPassword = db.user.password;
+        } 
+        else
+        {
+            Debug.LogError("db.json not found!");    
+        }
+    }
 
     public void ReadData()
     {
@@ -19,6 +54,10 @@ public class RegistrationScript : MonoBehaviour
         if (email == defaultEmail && password == defaultPassword)
         {
             SceneManager.LoadScene("MenuScene");
+        }
+        else
+        {
+            Debug.Log("The wrong email or password.");
         }
     }
 }
